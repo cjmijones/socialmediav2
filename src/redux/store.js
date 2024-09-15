b4f07b40486f5a0c1223c7db77744b7d04bcc7ab
cjmijones <cmj@uchicago.edu>
2024-09-14 20:14:02 -0400

    reset production environment

--- src/redux/store.js
+++ src/redux/store.js
@@ -33,7 +33,7 @@ export const store = configureStore({
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     }),
-  devTools: devMode,
+  devTools: false,
 });
 
 export const persistor = persistStore(store);
